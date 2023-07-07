3d4eed9bf4e683876657ce1194559633e4e91eef
Dario Quiroga <dario@kernelinformatica.com.ar>
2023-07-07 13:13:53 -0300

    Version 2: publicada en playstore

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.gestagro.app',
+   appId: 'ar.com.gestagro.gestagromovilcoop',
+  //appId: 'com.gestagro.app',
   appName: 'GestAgro',
   webDir: 'www',
   server: {
